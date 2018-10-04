486762380a92168a231e90de93dbe1a6863543a7
carson <carson42g@gmail.com>
2018-10-04 13:15:17 -0400

    Added some more stuff YAY most of this probably isn't useable, but i'm just playing around with the librarys
    whoever runs the github: can you please add a webhook from our repo to
    the link in
    TeamCode/src/main/java/org/firstinspires/ftc/teamcode/github_webhook_link.txt
    It's linked to a google script which is linked to a groupme bot account.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous1.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
 import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import java.util.concurrent.TimeUnit;
 
@@ -22,8 +23,8 @@ public class Autonomous1 extends LinearOpMode {
     private DcMotor rf_motor;
     private DcMotor rb_motor;
     private Servo knocker;
-    static final double forwardSpeed = .5;
-    private ElapsedTime runtime = new ElapsedTime();
+    static final double FORWARD_SPEED = .5;
+    private ElapsedTime runtime = new ElapsedTime();//starts timer
     ColorSensor sensey;
     HardwareMap hwMap = null;
 
@@ -48,23 +49,73 @@ public class Autonomous1 extends LinearOpMode {
         waitForStart();
         knocker.setPosition(1);
 //        telemetry.addData("RED", sensey.red());//replace with
-        telemetry.addData("RED",new Func<String>() {//lamba would have been used by java version is capped at 1.7 :(
+        setTelemetryColorDefaults(telemetry,sensey);
+
+        setAllDriveMoters(FORWARD_SPEED);
+        ElapsedTime eTime = new ElapsedTime();
+//        eTime.reset();//not needed, checkout the default constructor//REMOVE
+//        while (eTime.time() < 2.0) {//REMOVE (put in method)
+//             Thread.sleep(10);//poll time. don't make calls in instantly, waiting as little as 10 ms greatly deceases processing needed
+//        }
+
+        waitForETime(eTime,2 * 1000);//wait two seconds
+
+        setAllDriveMoters(0);//stop moving after 2 seconds, ~+10ms
+        //maybe?
+        telemetry.addData("Total runtime",runtime.time(TimeUnit.MILLISECONDS));
+        telemetry.update();
+
+        //turn?
+        //left
+        lf_motor.setPower(FORWARD_SPEED /2);
+        lb_motor.setPower(FORWARD_SPEED /2);
+
+        //right
+        rf_motor.setPower(-FORWARD_SPEED /2);
+        lf_motor.setPower(-FORWARD_SPEED /2);
+        eTime.reset();//reuse the same etime inst
+        waitForETime(eTime,1);
+
+        setAllDriveMoters(0);
+
+
+
+    }
+
+
+    private void waitForETime(ElapsedTime e,double ms,int msPollWait) throws InterruptedException {
+        while(e.time(TimeUnit.MILLISECONDS) < ms){
+            Thread.sleep(msPollWait);
+        }
+    }
+
+    private void waitForETime(ElapsedTime e,double ms) throws InterruptedException {
+        waitForETime(e,ms,10);
+    }
+
+
+    private void setTelemetryColorDefaults(Telemetry telemetry, final ColorSensor sensey) {
+        telemetry.addData("Red",new Func<String>() {//lamba would have been used by java version is capped at 1.7 :(
             @Override
             public String value() {
                 return sensey.red() + "";
             }
         });//sends red value everytime telemtry.update() is called TODO VERIFY THIS WORKS
-        setAllDriveMoters(forwardSpeed);
-        ElapsedTime eTime = new ElapsedTime();
-        eTime.reset();
-        while (eTime.time() < 2.0) {
-             Thread.sleep(10);//poll time. don't make calls in instantly, waiting as little as 10 ms greatly deceases processing needed
-        }
-        setAllDriveMoters(0);//stop moving after 2 seconds, ~+10ms
 
-        //maybe?
-        telemetry.addData("Runtime",runtime.time(TimeUnit.MILLISECONDS));
-        telemetry.update();
+
+        //other colors.. might be useful..or annoying. We'll see?
+        telemetry.addData("Blue",new Func<String>() {
+            @Override
+            public String value() {
+                return sensey.blue() + "";
+            }
+        });
+        telemetry.addData("Green",new Func<String>() {
+            @Override
+            public String value() {
+                return sensey.green() + "";
+            }
+        });
     }
 
     private void setAllDriveMoters(double speed){
@@ -73,6 +124,8 @@ public class Autonomous1 extends LinearOpMode {
         rf_motor.setPower(speed);
         rb_motor.setPower(speed);
     }
+
+
 
 }
 
