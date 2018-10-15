a2bee785a7be28d2c559918f4c425ccb9807c4cf
Bayan Rasooly <happybayan@gmail.com>
2018-10-15 19:14:37 -0400

    Eliminated code redundancies & useless servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest2.java
@@ -14,8 +14,6 @@ import static android.R.transition.move;
 @TeleOp(name="tele2", group="test")
 //@Disabled
 public class TeleOpTest2 extends LinearOpMode{
-
-
     private DcMotor lf_motor;
     private DcMotor lb_motor;
     private DcMotor rf_motor;
@@ -26,7 +24,7 @@ public class TeleOpTest2 extends LinearOpMode{
 
 
     HardwareMap hwMap =  null;
-
+    //test
     public void init(HardwareMap ahwMap) {
         // Save reference to Hardware map
         hwMap = ahwMap;
@@ -38,6 +36,61 @@ public class TeleOpTest2 extends LinearOpMode{
         lservo = hwMap.servo.get("lservo");
         rservo = hwMap.servo.get("rservo");
     }
+    public boolean moveForwards(float speed){
+        try{
+            lf_motor.setPower(speed);
+            lb_motor.setPower(speed);
+            rf_motor.setPower(speed);
+            rb_motor.setPower(speed);
+        }
+        catch(Throwable t){
+            return false;
+        }
+        return true;
+    }
+    public boolean moveBackwards(float speed){
+        try{
+            lf_motor.setPower(-speed);
+            lb_motor.setPower(-speed);
+            rf_motor.setPower(-speed);
+            rb_motor.setPower(-speed);
+        }
+        catch(Throwable t){
+            return false;
+        }
+        return true:
+    }
+    public boolean turnCounterclockwise(float speed){
+        try{
+            lf_motor.setPower(-speed);
+            lb_motor.setPower(speed);
+            rf_motor.setPower(-speed);
+            rb_motor.setPower(speed);
+        }
+        catch(Throwable t){
+            return false;
+        }
+        return true;
+    }
+    public boolean turnClockwise(float speed){
+        try{
+            lf_motor.setPower(speed);
+            lb_motor.setPower(-speed);
+            rf_motor.setPower(speed);
+            rb_motor.setPower(-speed);
+        }
+        catch(Throwable t){
+            return false;
+        }
+        return true;
+    }
+    public boolean pause(){
+        lf_motor.setPower(0.0);
+        lb_motor.setPower(0.0);
+        rf_motor.setPower(0.0);
+        rb_motor.setPower(0.0);
+        return true;
+    }
     @Override
     public void runOpMode() throws InterruptedException{
 
@@ -46,105 +99,30 @@ public class TeleOpTest2 extends LinearOpMode{
         telemetry.update();
         waitForStart();
         while(opModeIsActive()) {
-
-            //float straight;
-            //float sideways;
-            float orientation;
-
-            orientation = gamepad1.right_stick_y;
-
-            //lift.setPower(0.0);
-
-            //driveStraight(straight);
-            //driveSideways(sideways);
-
-            //turn(orientation);
-
-
-            if(gamepad2.a)
-                lift.setPower(1);
-            else if(gamepad2.b)
-                lift.setPower(-1);
-            else
-                lift.setPower(0);
-
-
-
-            if(gamepad2.y)
-                openServo(0);
-            if (gamepad2.x)
-                closeServo(.9);
-
-            //float speed = (gamepad1.left_trigger != 0)? SPEED_MULTIPLIER : TURBO_MULTIPLIER;
-
             float speed = (gamepad1.y)? 1f : 0.5f;
 
             if(gamepad1.right_trigger != 0)
             {
-                lf_motor.setPower(-speed);
-                lb_motor.setPower(-speed);
-                rf_motor.setPower(-speed);
-                rb_motor.setPower(-speed);
+                moveForwards(speed);
             }
             else if(gamepad1.left_trigger != 0)
             {
-                lf_motor.setPower(speed);
-                lb_motor.setPower(speed);
-                rf_motor.setPower(speed);
-                rb_motor.setPower(speed);
-            }
-            else if(gamepad1.dpad_up)
-            {
-                lf_motor.setPower(-speed);
-                lb_motor.setPower(-speed);
-                rf_motor.setPower(speed);
-                rb_motor.setPower(speed);
-            }
-            else if(gamepad1.dpad_down)
-            {
-                lf_motor.setPower(speed);
-                lb_motor.setPower(speed);
-                rf_motor.setPower(-speed);
-                rb_motor.setPower(-speed);
+                moveBackwards(speed);
             }
             else if(gamepad1.dpad_left)
             {
-                lf_motor.setPower(speed);
-                lb_motor.setPower(-speed);
-                rf_motor.setPower(speed);
-                rb_motor.setPower(-speed);
+                turnCounterclockwise(speed);
             }
             else if(gamepad1.dpad_right)
             {
-                lf_motor.setPower(-speed);
-                lb_motor.setPower(speed);
-                rf_motor.setPower(-speed);
-                rb_motor.setPower(speed);
-            } else {
-                lf_motor.setPower(0.0);
-                lb_motor.setPower(0.0);
-                rf_motor.setPower(0.0);
-                rb_motor.setPower(0.0);
+                turnClockwise(speed);
+            }
+            else {
+                pause();
             }
             idle();
         }
-        // Gwendyl Harter - have we tried putting an idle() at the end of
-        // the while op-mode loop?  Documentation says that this is important
-        // for giving the machinery time to catch up with the code.
-        // May explain the jittery-ness
     }
-    // public void driveStraight(float power) {
-    // lf_motor.setPower(-power);
-    // lb_motor.setPower(-power);
-    // rf_motor.setPower(-power);
-    // rb_motor.setPower(-power);
-    // }
-    //public void driveSideways(float power){
-    //  lf_motor.setPower(-power);
-    //  lb_motor.setPower(power);
-    //  rf_motor.setPower(-power);
-    //  rb_motor.setPower(power);
-    //  }
     public void closeServo(double position) {
         lservo.setPosition(position);
         rservo.setPosition(1-position);
@@ -153,12 +131,4 @@ public class TeleOpTest2 extends LinearOpMode{
         lservo.setPosition(position);
         rservo.setPosition(1-position);
     }
-
-
 }
-
-
-
-
-
-
