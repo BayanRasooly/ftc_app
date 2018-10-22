4f57affb94c6142263b0d0cf2b062268fd03849b
Bayan Rasooly <happybayan@gmail.com>
2018-10-22 19:07:23 -0400

    Reconstructed code oct 22

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/TeleOpTest2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/TeleOpTest2.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.tele;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
@@ -20,11 +21,23 @@ import static android.R.transition.move;
 @TeleOp(name="tele2", group="test")
 //@Disabled
 public class TeleOpTest2 extends LinearOpMode{
-    Robot robot;
-    Encoder en;
+    public DcMotor r_motor;
+    public DcMotor l_motor;
+
+    public DcMotor climb;
+    //do servos later
+    public Servo lb_servo;// Left Bar Servo
+    public Servo rb_servo;// Right Bar Servo
+
+    public ColorSensor left_sensey;
+    public ColorSensor right_sensey;
+    public DistanceSensor left_distance;
+    public  DistanceSensor right_distance;
+
     public boolean moveForwards(float speed){
         try{
-            en.setBothMotorPower(speed);
+            r_motor.setPower(speed);
+            l_motor.setPower(speed);
         }
         catch(RuntimeException t){
             t.printStackTrace();
@@ -33,19 +46,12 @@ public class TeleOpTest2 extends LinearOpMode{
         return true;
     }
     public boolean moveBackwards(float speed){
-        try{
-            en.setBothMotorPower(-speed);
-        }
-        catch(RuntimeException t){
-            t.printStackTrace();
-            return false;
-        }
-        return true;
+        return moveForwards(-speed);
     }
     public boolean turnCounterclockwise(float speed){
         try{
-            en.setRightMotorPower(speed);
-            en.setLeftMotorPower(-speed);
+            r_motor.setPower(speed);
+            l_motor.setPower(-speed);
         }
         catch(RuntimeException t){
             t.printStackTrace();
@@ -54,14 +60,7 @@ public class TeleOpTest2 extends LinearOpMode{
         return true;
     }
     public boolean turnClockwise(float speed){
-        try {
-            en.setRightMotorPower(-speed);
-            en.setLeftMotorPower(speed);
-        }
-        catch(RuntimeException t){
-            return false;
-        }
-        return true;
+        return turnCounterclockwise(-speed);
     }
     public boolean quickTurn(float degree){
         throw new AssertionError("Hasn't been written yet - can't be called");
@@ -77,11 +76,12 @@ public class TeleOpTest2 extends LinearOpMode{
         return true;
     }
 
+    @Deprecated
     public boolean align(){
 //        double margin = 0.50;
 //        while(rDistanceSensor.getDistance(DistanceUnit.INCH))
         try {
-            en.align();//margin is set within the method
+//            en.align();//margin is set within the method
         }catch(RuntimeException e){
             e.printStackTrace();
             return false;
@@ -89,10 +89,22 @@ public class TeleOpTest2 extends LinearOpMode{
         return true;
     }
 
+    private void initMap(){
+        r_motor = hardwareMap.dcMotor.get("Right Back Motor");
+        l_motor = hardwareMap.dcMotor.get("Left Motor");
+        climb = hardwareMap.dcMotor.get("Climbing Motor");
+        lb_servo = hardwareMap.servo.get("Left Bar Motor");
+        rb_servo = hardwareMap.servo.get("Right Bar Motor");
+        left_sensey = hardwareMap.colorSensor.get("Left Color Sensor");
+        right_sensey = hardwareMap.colorSensor.get("Right Color Sensor");
+        left_distance = (DistanceSensor) hardwareMap.opticalDistanceSensor.get("Left Distance Sensor");
+        right_distance = (DistanceSensor) hardwareMap.opticalDistanceSensor.get("Right Distance Sensor");
+    }
+
+
     @Override
     public void runOpMode() {
-        robot = new Robot(hardwareMap);
-        en = new Encoder(robot,this);
+        initMap();
         telemetry.addData("Status", "Ready to run");
         telemetry.update();
         waitForStart();
