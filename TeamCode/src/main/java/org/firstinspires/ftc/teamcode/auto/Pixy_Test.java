32c1d627377947928b1bab6865df20fa713501ba
LAPTOP-G9DI5QNL\kevin <alex.yusman@yahoo.com>
2018-10-24 14:43:22 -0400

    Correct Pixy Reads

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Pixy_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Pixy_Test.java
@@ -2,21 +2,58 @@ package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import org.firstinspires.ftc.teamcode.core.Robot;
+
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.AnalogInputController;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.SerialNumber;
+
+import org.firstinspires.ftc.robotcore.external.Func;
 
 
-@Autonomous(name="Claim Zone Auto", group="Robot")
+@Autonomous(name="Pixy_Test", group="Robot")
 
 public class Pixy_Test extends LinearOpMode {
-    Robot robot;
-    double volts;
+
+    HardwareMap hwMap = null;
+    private AnalogInputController controller = null;
+    AnalogInput controller1 = new AnalogInput(controller,0);
+
+    public void init(HardwareMap ahwMap) {
+        hwMap = ahwMap;
+        controller1 = hwMap.analogInput.get("ana");
+
+    }
+            /*
+    //AnalogInputController ana;
+*/
     public void runOpMode() throws InterruptedException {
-        robot = new Robot(hardwareMap);
+        init(hardwareMap);
         telemetry.addData("Status", "Ready to run");
         telemetry.update();
+        waitForStart();
+        //if connect
+        telemetry.addData("connection info", controller1.getConnectionInfo());
+        //check voltage
+        telemetry.addData("Volts",new Func<String>(){
+            @Override
+            public String value() {
+                return controller1.getVoltage() + "";
+            }
+        });
+        //check iterations/runs
+        telemetry.addData("Iteration",new Func<String>(){
+            int index = 0;
+            @Override
+            public String value() {
+                return index++ + "";
+            }
+        });
+
         while(opModeIsActive()) {
-            double volts = robot.ana.getVoltage();
-            telemetry.addData("Volts", volts);
+            //push telemetry to phone
+            telemetry.update();
+            Thread.sleep(10);
         }
     }
 }
