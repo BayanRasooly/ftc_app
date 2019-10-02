dcd6b6353eb049cd15ee80e4fa5eb2c6d01ce3f0
Chris <plasmaticdarkness@gmail.com>
2019-10-02 18:19:41 -0400

    no message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/TestingTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/TestingTele.java
@@ -6,16 +6,25 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class TestingTele extends LinearOpMode
 {
     private DcMotor motorLeft;
+    private DcMotor motorRight;
+    private DcMotor motorRearLeft;
+    private DcMotor motorRearRight;
 
     @Override
     public void runOpMode()
     {
         motorLeft = hardwareMap.dcMotor.get("motorLeft");
+        motorRight = hardwareMap.dcMotor.get("motorRight");
+        motorRearLeft = hardwareMap.dcMotor.get("motorRearLeft");
+        motorRearRight = hardwareMap.dcMotor.get("motorRearRight");
 
         waitForStart();
 
         while (opModeIsActive())
 
             motorLeft.setPower(1);
+            motorRight.setPower(1);
+            motorRearLeft.setPower(1);
+            motorRearRight.setPower(1);
     }
 }
